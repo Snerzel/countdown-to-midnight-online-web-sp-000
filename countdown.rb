dfcf81b20453cde64e8c63ffdce466b8802ade83
Cherissa Woudenberg <snerzel@comcast.net>
2021-03-24 16:22:13 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
+#counter = 10
 
-def countdown
+#def countdown(counter)
+
+#  while counter > 0
+  #  puts "#{counter} SECOND(S)!"
+    #counter -= 1
+    #puts "HAPPY NEW YEAR!"
+  #end
+  #puts "HAPPY NEW YEAR!"
+#end
+counter = 10
+def countdown(counter)
+loop do
+  counter -= 1
+  puts "#{counter} SECOND(S)!"
+  if counter > 0
+    break
+    puts "HAPPY NEW YEAR!"
+
+    end
+  end
 end
